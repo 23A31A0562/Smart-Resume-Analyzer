626b41f2054a8cef6548bf1bcb9bc5965873f706
Bhargav vipparthi <bhargavvipparthi@gmail.com>
2026-01-14 21:00:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,10 @@ def index():
         error=error
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
